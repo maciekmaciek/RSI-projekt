4dd17b1848b73b9e8e706ae0c072ce8c7c4c254b
Monika Nowakowicz <m.nowakowicz@gmail.com>
2015-05-10 14:17:21 +0200

    MPI bugfix'd

--- Main.py
+++ Main.py
@@ -2,52 +2,54 @@ __author__ = 'Monis'
 
 from properties import Properties
 from PIL import Image
+from operator import add
+import time
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-#TODO images adresses
+# TODO images adresses
 images = [Image.open("./goku1.jpg"), Image.open("./goku2.jpg"), Image.open("./goku1.jpg"), Image.open("./goku2.jpg"),
           Image.open("./goku1.jpg"), Image.open("./goku2.jpg"), Image.open("./goku2.jpg")]
 
 if rank == 0:
+    start = time.time()
     images_count = images.__len__()
     size = comm.Get_size()
     rank_images = images_count / size
-    print(rank_images)
 
     for send_rank in range(1, size):
         send_images = []
-        for part_image in range(0, rank_images - 1):
-            print(part_image, "   rang_imgs:", rank_images, "  size: ", size, images_count)
+        for part_image in range(0, rank_images):
             send_images.append(images[0])
             images.remove(images[0])
         comm.send(send_images, dest=send_rank, tag=111)
 
-    propertiesList = []
-    for recv_rank in range(1, size):
-        properties = comm.recv(source=recv_rank, tag=111)
-        propertiesList.append(properties)
-        print('odebral')
-
+    rank0_images = images.__len__()
     p = Properties()
     p.load(images)
     properties = p.get_properties()
-    propertiesList.append(properties)
+    trait = properties[0] * rank0_images
+    dynamic = properties[1] * rank0_images
+    colors = [x * rank0_images for x in properties[2]]
 
-    trait = 0.
-    dynamic = 0.
-    colors = []
+    for recv_rank in range(1, size):
+        properties = comm.recv(source=recv_rank, tag=111)
+        trait += properties[0] * rank_images
+        dynamic += properties[1] * rank_images
+        map(add, colors, [x * rank_images for x in properties[2]])
 
-    for part in range(0, size):
-        trait += propertiesList[part][0]
-        dynamic += propertiesList[part][1]
-        colors.extend(propertiesList[part][2])
+    colors_map = [(0.125, "red"), (0.25, "orange"), (0.375, "yellow"),
+                  (0.5, "green"), (0.625, "teal"), (0.75, "blue"),
+                  (0.875, "purple"), (1., "pink")]
 
-    print('Srednia jasnosc: {0}'.format(trait / size))
-    print('Srednia dynamika: {0}'.format(dynamic / size))
-    #print('Sredni kolor: {0}'.format(color))
+    print('WYNIKI:')
+    print('Srednia jasnosc: {0}'.format(trait / images_count))
+    print('Srednia dynamika: {0}'.format(dynamic / images_count))
+    print('Dominujacy kolor: {0}'.format(colors_map[colors.index(max(colors))][1]))
+    end = time.time()
+    print('Czas: {0}'.format(end - start))
 else:
     images = comm.recv(source=0, tag=111)
     p = Properties()
--- properties.py
+++ properties.py
@@ -1,6 +1,6 @@
 # coding=utf-8
-from PIL import Image
 import colorsys
+
 
 class Properties:
     colors = [(0.125, "red"), (0.25, "orange"), (0.375, "yellow"),
@@ -11,73 +11,68 @@ class Properties:
         self.images = []
         self.pix = []
 
-    def load(self):
-        #self.images = [Image.open("./goku1.jpg"), Image.open("./goku2.jpg")]
-        for image in self.images:
-            self.pix.append(image.load())
-
     def load(self, images):
         self.images = images
         for image in self.images:
             self.pix.append(image.load())
 
     def rgb_int_to_percent(self, rgb):
-            d_tup = (float(rgb[0])/255, float(rgb[1])/255, float(rgb[2])/255)
-            return d_tup
+        d_tup = (float(rgb[0]) / 255, float(rgb[1]) / 255, float(rgb[2]) / 255)
+        return d_tup
 
-#-------------------------------------------------- średnia jasność
+    # -------------------------------------------------- średnia jasność
 
     def trait_avg_value(self, images):
         avg = 0.
         for image in images:
             avg += self.v_in_image(image)
-        return avg/len(images)
+        return avg / len(images)
 
     def v_in_image(self, image):
         avgv = 0.
         pixels = image.load()
         for i in xrange(image.size[0]):
             for j in xrange(image.size[1]):
-                per = self.rgb_int_to_percent(pixels[i,j])
-                value = colorsys.rgb_to_hsv(per[0],per[1],per[2])[2]
+                per = self.rgb_int_to_percent(pixels[i, j])
+                value = colorsys.rgb_to_hsv(per[0], per[1], per[2])[2]
                 avgv += value
 
-        return avgv/image.size[0]/image.size[1]
+        return avgv / image.size[0] / image.size[1]
 
 
-#--------------------------------------------------średnia dynamika
+    # --------------------------------------------------średnia dynamika
 
     def trait_avg_value_change(self, images):
         avg = 0.
         size = len(images)
         if size > 1:
             for i in range(1, size):
-                avg += self.avg_pair_value_change(images[i-1], images[i])
-            return avg/(size-1)
+                avg += self.avg_pair_value_change(images[i - 1], images[i])
+            return avg / (size - 1)
         return 0
 
-    def avg_pair_value_change(self, image, image1): #ten sam rozmiar!
+    def avg_pair_value_change(self, image, image1):  # ten sam rozmiar!
         avgv = 0.
         pixels = image.load()
         pixels1 = image1.load()
         for i in xrange(image.size[0]):
             for j in xrange(image.size[1]):
-                avgv += self.pixel_value_change(pixels[i,j], pixels1[i,j])
-        return avgv/image.size[0]/image.size[1]
+                avgv += self.pixel_value_change(pixels[i, j], pixels1[i, j])
+        return avgv / image.size[0] / image.size[1]
 
     def pixel_value_change(self, pixel, pixel1):
         per = self.rgb_int_to_percent(pixel)
-        h = colorsys.rgb_to_hsv(per[0],per[1],per[2])[2]
+        h = colorsys.rgb_to_hsv(per[0], per[1], per[2])[2]
         per = self.rgb_int_to_percent(pixel1)
-        h1 = colorsys.rgb_to_hsv(per[0],per[1],per[2])[2]
-        return abs(h1-h)
+        h1 = colorsys.rgb_to_hsv(per[0], per[1], per[2])[2]
+        return abs(h1 - h)
 
-#----------------------------------------------------------najczęstszy kolor
+    # ----------------------------------------------------------najczęstszy kolor
     def trait_dominating_color(self, images):
         results = self.dominating_image_colors(images[0])
         for i in range(1, len(images)):
             results = [x + y for x, y in zip(results, self.dominating_image_colors(images[i]))]
-        return self.colors[results.index(max(results))]
+        return results
 
     def dominating_image_colors(self, image):
         results = [0, 0, 0, 0, 0, 0, 0, 0]
@@ -87,7 +82,7 @@ class Properties:
                 results[self.assign_pixel_to_color(pixels[i, j])] += 1
         return results
 
-    def assign_pixel_to_color(self,pixel):
+    def assign_pixel_to_color(self, pixel):
         per = self.rgb_int_to_percent(pixel)
         hue = colorsys.rgb_to_hsv(per[0], per[1], per[2])[0]
         return int((hue - 0.0625) * 8)
